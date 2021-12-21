90abdde1ad08a4a5a5b72262eb16feb160056125
vinicius-hora <vinicius_hora@live.com>
2021-12-21 15:34:07 -0300

    agradecer por novos pull requests

--- src/action.js
+++ src/action.js
@@ -1,5 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { context } = require('@actions/github/lib/utils');
+
 async function go(){
-    console.log('Olá, mundo!');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+    const octokit = new github.GitHub(GITHUB_TOKEN);
+
+    const { contex = {} } = github;
+    const { pull_request } = contex.payload;
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: 'Obrigado por submeter um pull request!'
+    });
 }
 
 go();
