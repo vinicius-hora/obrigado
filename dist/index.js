80dfac417a88c3a0fd2c4636a61982371d5005a4
vinicius-hora <vinicius_hora@live.com>
2021-12-21 16:34:08 -0300

    cirrigindo erro Error: Cannot find module 'node:http'1.1

--- dist/index.js
+++ dist/index.js
@@ -15262,9 +15262,8 @@ async function go(){
 
     const randomPos = Math.round(Math.random() * 1000);
     const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-
     const response = await fetch(url);
-    const { results } = await response.json(); 
+    const { results } = await response.json();
     const gifUrl = results[0].media[0].tinygif.url;
 
     const octokit = new github.GitHub(GITHUB_TOKEN);
