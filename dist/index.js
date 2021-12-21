9525186b36c9f0aed20de65305d15f7b35ddd077
vinicius-hora <vinicius_hora@live.com>
2021-12-21 15:09:21 -0300

    teste github actions

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+async function go(){
+    console.log('Olá, mundo!');
+}
+
+go();
+module.exports = __webpack_exports__;
+/******/ })()
+;
---
+++ src/action.js
@@ -0,0 +1,5 @@
+async function go(){
+    console.log('Olá, mundo!');
+}
+
+go();
